ace389141be2d788ca314a99e0af690778af086e
shortpeoplearesmarter <ajgerasydykova2@gmail.com>
2024-07-17 19:49:50 -0500

    update

--- Java.java
+++ Java.java
@@ -3,6 +3,7 @@ class Hello{
 public static void main (String [] args){
 System.out.println("Hello World");
   System.out.println("Practice fetch");
+  System.out.println("Another practice fetch");
 }
 
 } 
