8435fba4a9bd8275ed0550b40ca8e3aa90894770
Gera <gera@MBP-Gera.hsd1.il.comcast.net>
2024-07-17 20:16:14 -0500

    Revert "added printl"
    This reverts commit 7e2fde4c9605d5c4b6c7f922716d18eb5721d6c0.

--- Java.java
+++ Java.java
@@ -1,9 +1,8 @@
-class Hello{
+dfclass Hello{
 
 public static void main (String [] args){
 System.out.println("Hello World");
-  System.out.println("Practice fetch");
-  System.out.println("Another practice fetch");
+int a = 5;
 }
 
 } 
